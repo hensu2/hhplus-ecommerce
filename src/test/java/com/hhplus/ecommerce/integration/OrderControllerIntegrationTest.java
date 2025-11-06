acba1bfd749371c6fbfeb820ba0d71ddbb5388c6
HyeonSuJeong <hensu3846@gmail.com>
2025-11-07 07:41:06 +0900

    feat : jococo 추가 및 단위테스트 수정

--- src/test/java/com/hhplus/ecommerce/integration/OrderControllerIntegrationTest.java
+++ src/test/java/com/hhplus/ecommerce/integration/OrderControllerIntegrationTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -20,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 @DisplayName("OrderController 통합 테스트")
 class OrderControllerIntegrationTest {
 
--- src/test/java/com/hhplus/ecommerce/integration/ProductControllerIntegrationTest.java
+++ src/test/java/com/hhplus/ecommerce/integration/ProductControllerIntegrationTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.*;
@@ -15,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 @DisplayName("ProductController 통합 테스트")
 class ProductControllerIntegrationTest {
 
